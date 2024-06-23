636ee94acb9b723b72f589f52513485f982854ba
SaiNarsimhaGupta <venkatsai0398@gmail.com>
2024-06-23 21:15:47 +0530

    Initial Commit

---
+++ SpringIntegrationSamples/src/main/java/com/sainarsimhagupta/springintegration/SpringIntegrationSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.sainarsimhagupta.springintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationSamplesApplication.class, args);
+    }
+
+}
---
+++ SpringIntegrationSamples/src/test/java/com/sainarsimhagupta/springintegration/SpringIntegrationSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sainarsimhagupta.springintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
